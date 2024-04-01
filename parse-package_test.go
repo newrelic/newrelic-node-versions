66ead91bf81e330d3024ea69984674d0e22aa985
James Sumners <jsumners@newrelic.com>
2024-04-01 12:35:35 -0400

    add langchain test

--- parse-package_test.go
+++ parse-package_test.go
@@ -25,3 +25,21 @@ func Test_ParsePackage_Elastic(t *testing.T) {
 		assert.Equal(t, expected, found)
 	})
 }
+
+func Test_ParsePackage_Langchain(t *testing.T) {
+	var pkgJson VersionedTestPackageJson
+	file, err := os.ReadFile("testdata/versioned/langchain/package.json")
+	require.Nil(t, err)
+	err = json.Unmarshal(file, &pkgJson)
+	require.Nil(t, err)
+
+	t.Run("parses to correct representation", func(t *testing.T) {
+		expected := &PkgInfo{
+			Name:       "@langchain/core",
+			MinVersion: "0.1.17",
+		}
+		found, err := parsePackage(pkgJson)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, found)
+	})
+}
